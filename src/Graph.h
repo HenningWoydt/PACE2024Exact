1adca3d5fd5ae31fc4567454962985d118e4a028
henning <henningmartin.woydt@uni-jena.de>
2024-05-01 10:37:34 +0200

    * improved reading

--- src/Graph.h
+++ src/Graph.h
@@ -12,75 +12,128 @@
 #include "misc.h"
 
 /**
- * Class to store the bipartite m_graph.
+ * Class to store the bipartite graph.
  */
 class Graph {
 public:
     int m_n_A = 0;
     int m_n_B = 0;
+    int m_n_edges = 0;
 
     std::vector<std::vector<int>> m_adj_list;
 
     /**
-     * Reads a m_graph from a file.
+     * Reads a graph from a file.
      *
      * @param file_path Path to the file.
      */
     explicit Graph(std::string &file_path) {
+        std::ios_base::sync_with_stdio(false);
         std::ifstream file(file_path);
 
-        bool is_p_line = true;
+        std::string line(64, ' ');
         if (file.is_open()) {
-            for (std::string line; std::getline(file, line);) {
+            while (std::getline(file, line)) {
+                if (line[0] == 'c') { continue; }
+                if (line.back() == '\n' || line.back() == '\r') { line.pop_back(); }
 
-                if (line[0] == 'c') {
-                    continue;
+                assert(line[0] == 'p');
+                assert(line[1] == ' ');
+                assert(line[2] == 'o');
+                assert(line[3] == 'c');
+                assert(line[4] == 'r');
+                assert(line[5] == ' ');
+
+                m_n_A = 0;
+                m_n_B = 0;
+                m_n_edges = 0;
+                size_t i = 6;
+
+                // read in n_A
+                while(line[i] != ' '){
+                    m_n_A = m_n_A * 10 + line[i] - '0';
+                    i += 1;
                 }
-                if (line.back() == '\n' || line.back() == '\r') {
-                    line.pop_back();
+                i += 1;
+
+                // read in n_B
+                while(line[i] != ' '){
+                    m_n_B = m_n_B * 10 + line[i] - '0';
+                    i += 1;
                 }
 
-                if (is_p_line) {
-                    assert(line[0] == 'p');
-                    assert(line[1] == ' ');
-                    assert(line[2] == 'o');
-                    assert(line[3] == 'c');
-                    assert(line[4] == 'r');
-                    assert(line[5] == ' ');
-
-                    size_t i = 6;
-                    std::string s_A;
-                    std::string s_B;
-                    for (; line[i] != ' '; ++i) {
-                        s_A.push_back(line[i]);
-                    }
-                    ++i;
-                    for (; line[i] != ' '; ++i) {
-                        s_B.push_back(line[i]);
-                    }
-
-                    m_n_A = std::stoi(s_A);
-                    m_n_B = std::stoi(s_B);
-
-                    m_adj_list.resize(m_n_B);
-
-                    is_p_line = false;
-                } else {
-                    std::stringstream ss(line);
-                    int a, b;
-                    ss >> a >> b;
-                    b -= (m_n_A + 1);
-                    m_adj_list[b].push_back(a - 1);
+                m_adj_list.resize(m_n_B);
+                for(int j = 0; j < m_n_B; ++j){
+                    m_adj_list[j].reserve(16);
                 }
+                break;
+            }
+
+            while (std::getline(file, line)) {
+                if (line[0] == 'c') { continue; }
+                if (line.back() == '\n' || line.back() == '\r') { line.pop_back(); }
+
+                int a = 0, b = 0;
+                size_t i = 0;
+
+                // read in vertex a
+                while(line[i] != ' '){
+                    a = a * 10 + line[i] - '0';
+                    i += 1;
+                }
+                i += 1;
+
+                // read in vertex b
+                while(i < line.size()){
+                    b = b * 10 + line[i] - '0';
+                    i += 1;
+                }
+
+                b -= (m_n_A + 1);
+                add_edge(a, b);
             }
             file.close();
 
             // sort each neighborhood
-            for(int i = 0; i < m_n_B; ++i){
-                std::sort(m_adj_list[i].begin(), m_adj_list[i].end());
-            }
+            sort_neighborhoods();
         } else {
             std::cout << "Could not open file " << file_path << " !" << std::endl;
+        }
+    }
+
+    /**
+     * Initializes the graph.
+     *
+     * @param n_A Number of fixed vertices.
+     * @param n_B Number of movable vertices.
+     */
+    Graph(int n_A, int n_B){
+        m_n_A = n_A;
+        m_n_B = n_B;
+        m_n_edges = 0;
+
+        m_adj_list.resize(m_n_B);
+    }
+
+    /**
+     * Adds an edge to the graph. Note that a must be in the range
+     * {0, ..., n_A - 1} and b must be in the range {0, ..., n_B - 1}.
+     *
+     * @param a Vertex a (fixed vertex).
+     * @param b Vertex b (movable vertex).
+     */
+    void add_edge(int a, int b){
+        m_n_edges += 1;
+        m_adj_list[b].push_back(a);
+    }
+
+    /**
+     * Sorts each neighborhood ascending.
+     */
+    void sort_neighborhoods(){
+        // sort each neighborhood
+        for(int i = 0; i < m_n_B; ++i){
+            std::sort(m_adj_list[i].begin(), m_adj_list[i].end());
         }
     }
 
@@ -98,19 +151,13 @@ public:
                 int b1 = permutation[i];
                 int b2 = permutation[j];
 
-                int b1_pos = i;
-                int b2_pos = j;
-
                 // loop through the edges
                 for (size_t k = 0; k < m_adj_list[b1].size(); ++k) {
                     for (size_t l = 0; l < m_adj_list[b2].size(); ++l) {
                         int a1_pos = m_adj_list[b1][k];
                         int a2_pos = m_adj_list[b2][l];
 
-                        bool cut1 = (a1_pos < a2_pos) && (b2_pos < b1_pos);
-                        bool cut2 = (a2_pos < a1_pos) && (b1_pos < b2_pos);
-                        bool cut = cut1 || cut2;
-                        n_cuts += cut;
+                        n_cuts += (a2_pos < a1_pos);
                     }
                 }
             }
@@ -118,6 +165,10 @@ public:
         return n_cuts;
     }
 
+    /**
+     * Prints the graph as an adjacency list. Only prints the adjacency of
+     * movable nodes.
+     */
     void print() const {
         for(int i = 0; i < m_n_B; ++i) {
             std::cout << i << ": ";
