8ac68fac613d46c2c4c8becdbeba4a71583f5ab2
henning <henningmartin.woydt@uni-jena.de>
2024-06-10 08:13:43 +0200

    * fixed name in script

--- experiments/compile_project.py
+++ experiments/compile_project.py
@@ -26,8 +26,8 @@ def compile_project() -> None:
     """
     subprocess.call("mkdir -p ../build", shell=True, stdout=subprocess.PIPE)
     subprocess.call("cd .. && cmake -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release -S . -B ./build", shell=True, stdout=subprocess.PIPE)
-    subprocess.call("cd .. && cmake --build build --target PACE2024Exact", shell=True, stdout=subprocess.PIPE)
-    subprocess.call("cd .. && chmod +x build/PACE2024Exact", shell=True, stdout=subprocess.PIPE)
+    subprocess.call("cd .. && cmake --build build --target crossingguard", shell=True, stdout=subprocess.PIPE)
+    subprocess.call("cd .. && chmod +x build/crossingguard", shell=True, stdout=subprocess.PIPE)
 
 
 if __name__ == '__main__':
